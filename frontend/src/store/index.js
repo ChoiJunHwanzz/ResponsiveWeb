9077ee377ef6e4c7d092a67328f273656658a148
ChoiJunHwanzz <wnsghks884@gmail.com>
2021-12-29 19:48:43 +0900

    front 에러잡는중

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import state from './states'
+import actions from './actions'
+import mutations from './mutations'
+import getters from './getters'
+// import createPersistedState from 'vuex-persistedstate'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  getters: {
-  }
+  // plugins: [createPersistedState()],
+  state,
+  actions,
+  mutations,
+  getters,
 })

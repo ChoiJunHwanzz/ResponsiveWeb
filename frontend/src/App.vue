ee5cf09935f085f92b0c006f557130f1012c5e03
ChoiJunHwanzz <wnsghks884@gmail.com>
2022-01-04 00:02:56 +0900

    navbar 작업중

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,42 +1,24 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        네비바 설정
-      </div>
-
-      <v-spacer></v-spacer>
-
-     
+  <div>
+    <v-app class="test">
+      <nav-bar-component/>
+        <v-main>
+          <router-view/>
+        </v-main>
         
-    </v-app-bar>
-
-    <v-main>
-      <router-view/>
-    </v-main>
-  </v-app>
+    </v-app>
+  </div>
 </template>
 
 <script>
+import NavBarComponent from './components/navbar/NavBarComponent.vue'
 
 export default {
-  name: 'App',
-
-  data: () => ({
-    //
-  }),
-};
+  components: { NavBarComponent },
+  name: 'App'
+}
 </script>
+
+<style>
+  
+</style>
---
+++ frontend/src/components/navbar/NavBarComponent.vue
@@ -0,0 +1,76 @@
+<template>
+    <div id="app">
+  <v-app id="inspire">
+      <v-app-bar
+        absolute
+        color="#6A76AB"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-4"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>
+  
+  
+        <v-toolbar-title>Relative Web</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn color="transparent" elevation="0">
+            로그인
+        </v-btn>
+  
+        <v-btn color="transparent" elevation="0">
+            회원가입
+        </v-btn>
+  
+        
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab>Tab 1</v-tab>
+            <v-tab>Tab 2</v-tab>
+            <v-tab>Tab 3</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+      
+  </v-app>
+</div>
+</template>
+
+<script>
+
+
+
+export default {
+    data () {
+        return {
+            items: [
+                    { title: 'Click Me' },
+                    { title: 'Click Me' },
+                    { title: 'Click Me' },
+                    { title: 'Click Me 2' },
+                    ],
+        }
+    },    
+}
+</script>
+
+<style scoped>
+h1 {
+    margin: 10px;
+}
+p {
+    padding: 10px;
+    margin: 5px 10px;
+}
+</style>
+
+
